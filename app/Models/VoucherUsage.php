ba957ccf8c336c1042f8c9905199014e24ea2708
Mofuuwu <Jeangunhildr26@gmail.com>
2025-02-11 11:46:23 +0700

    menambahkan migrasi tabel

---
+++ app/Models/VoucherUsage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VoucherUsage extends Model
+{
+    /** @use HasFactory<\Database\Factories\VoucherUsageFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2025_02_11_043138_create_vouchers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->enum('type', ['fixed', 'percentage']);
+            $table->decimal('value');
+            $table->date('valid_from');
+            $table->date('valid_until');
+            $table->integer('amount');
+            $table->integer('remaining');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
